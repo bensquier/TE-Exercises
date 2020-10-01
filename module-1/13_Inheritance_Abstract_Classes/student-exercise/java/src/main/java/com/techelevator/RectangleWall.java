f58c997f996779708e0326ac103fd480d49b559d
Ben Squier <squier.ben@gmail.com>
2020-10-01 13:30:21 -0400

    Completed Inheritance Abstract Classes Exercises

---
+++ module-1/13_Inheritance_Abstract_Classes/student-exercise/java/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,32 @@
+package com.techelevator;
+
+public class RectangleWall extends Wall {
+	
+	private int length;
+	private int height;
+	
+	public int getLength() {
+		return length;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public RectangleWall(String name, String color, int length, int height) {
+		super(name, color);
+		this.length = length;
+		this.height = height;
+	}
+
+	@Override
+	public int getArea() {
+		return length * height;
+	}
+	
+	public String toString() {
+		return String.format("%s (%dx%d) rectangle", super.getName(), length, height);
+		
+	}
+
+}
