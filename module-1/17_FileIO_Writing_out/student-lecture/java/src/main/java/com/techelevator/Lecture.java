d3a090cf4c8c4a4e138ab8c9f7775152aa7979a1
Ben Squier <squier.ben@gmail.com>
2020-10-06 12:56:13 -0400

    File IO Writing Lecture Notes

--- module-1/17_FileIO_Writing_out/student-lecture/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/17_FileIO_Writing_out/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -19,9 +19,48 @@ public class Lecture {
 		 *
 		 * A new instance of File can be created from a String that contains a file system path
 		 */
+		
+		System.out.println("Enter file or path to inspect");
+		String path = userInput.nextLine();
+		
+		File file = new File(path);
+		
+		System.out.println(file.toString());
 
-
-
+		if(file.exists()) {
+			
+			System.out.println("Name: " + file.getName());
+			
+			System.out.println("Absolute Path: " + file.getAbsolutePath());
+			
+			if(file.isDirectory()) {
+				System.out.println(path + " is a directory");
+			}else if(file.isFile()) {
+				System.out.println(path + "is a file");
+			}
+			
+			System.out.println("size " + file.length());
+			
+		}
+		else {
+			System.out.println("I couldn't find that file:" + path);
+		}
+		
+		System.out.println("What is the name of the New directory you want to create");
+		String directory = userInput.nextLine();
+		
+		File newDirectory = new File(directory);
+		
+		if(newDirectory.exists()) {
+			System.out.println("Sorry, I can not create that directory (" + directory + "), it already exists");
+		}else {
+			if(newDirectory.mkdir()) {
+				System.out.println(directory + " was created");
+			}else {
+				System.out.println("Sorry could not create " + directory);
+			}
+		}
+		
 	}
 
 }
