5c74be60672da8ee5df4edbd9fad1cef5657e7d2
Ben Squier <squier.ben@gmail.com>
2020-10-06 17:36:19 -0400

    Completed FizzWriter For FileIO Writing

--- module-1/17_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
@@ -1,8 +1,46 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		String fileName = "FizzBuzz.txt";
+		
+		File fizzWriter = new File(fileName);
+		
+		fizzWriter.createNewFile();
+		
+		try(PrintWriter writer = new PrintWriter(fizzWriter)) {
+			for(int i = 1; i <= 300; i++) {
+				
+			String numberAsString = new Integer(i).toString();
+			
+				if(i % 3 == 0 && i % 5 == 0) {
+					writer.println("FizzBuzz");
+				}
+				else if(i % 5 == 0) {
+					writer.println("Buzz");
+				}
+				else if(i % 3 == 0) {
+					writer.println("Fizz");
+				}
+				else if(numberAsString.contains("5")) {
+					writer.println("Buzz");
+				}
+				else if(numberAsString.contains("3")) {
+					writer.println("Fizz");
+				}
+				else {
+					writer.println(i);
+				}
+			}
+			System.out.println("FizzBuzz.txt has been created");
+		}
 
 	}
 
