fe80067253d4b7fb1e63a49e1c8d4df97fca4bf4
Ben Squier <squier.ben@gmail.com>
2020-10-19 19:16:11 -0400

    Completed Week 1 Review Exercise

--- module-2/Week1_Review/student-exercise/postgres/step-4.sql
+++ module-2/Week1_Review/student-exercise/postgres/step-4.sql
@@ -1,11 +1,35 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
 
+SELECT 
+        p.name, c.name, c.open_from_mm, c.open_to_mm, c.daily_fee 
+FROM 
+        park p 
+                INNER JOIN campground c 
+                        ON p.park_id = c.park_id 
+ORDER BY p.name ASC, c.name ASC; 
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
-
+SELECT
+        p.name, COUNT(c.park_id)
+FROM
+        park p 
+                INNER JOIN campground c
+                        ON p.park_id = c.park_id
+GROUP BY p.name
+ORDER BY p.name;
 
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
+
+SELECT 
+        p.name, c.name, s.site_number, s.max_occupancy, accessible, max_rv_length, utilities
+FROM 
+        park p
+                INNER JOIN campground c
+                        ON p.park_id = c.park_id
+                                INNER JOIN site s
+                                        ON c.campground_id = s.campground_id
+WHERE c.name = 'Blackwoods';
 
 /*
   select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
@@ -20,9 +44,27 @@
   -------------------------------------------------
 */
 
+SELECT 
+        p.name, c.name, COUNT(site_number)
+FROM
+        park p 
+                INNER JOIN campground c
+                        ON p.park_id = c.park_id
+                                INNER JOIN site s
+                                        ON c.campground_id = s.campground_id
+GROUP BY c.name, p.name
+ORDER BY p.name ASC, COUNT(site_number) DESC;
+
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
 
+SELECT 
+        s.site_number, r.name, r.from_date, r.to_date
+FROM 
+        site s
+                INNER JOIN reservation r
+                        ON s.site_id = r.site_id
+ORDER BY r.from_date;
 
 
 /*
@@ -37,6 +79,17 @@
     Juniper Group Site	4
   -------------------------------------------------
 */
+
+SELECT 
+        c.name, COUNT(r.site_id)
+FROM
+        campground c
+                INNER JOIN site s
+                        ON c.campground_id = s.campground_id
+                                INNER JOIN reservation r
+                                        ON s.site_id = r.site_id
+GROUP BY c.name
+ORDER BY COUNT(r.site_id) DESC;
 
 
 
--- module-2/Week1_Review/student-exercise/postgres/step-5.sql
+++ module-2/Week1_Review/student-exercise/postgres/step-5.sql
@@ -10,12 +10,30 @@
   ------------------------------
 */
 
+BEGIN TRANSACTION;
+
+INSERT INTO 
+        park (name, location, establish_date, area, visitors, description)
+VALUES
+        ('Ohiopyle State Park', 'Pennsylvania', '1965-01-01'::DATE, 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.');
+
+COMMIT;
+
+ROLLBACK;
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
 */
 
+BEGIN TRANSACTION;
+
+UPDATE park SET visitors = 1500000
+WHERE name = 'Ohiopyle State Park';
+
+COMMIT;
+
+ROLLBACK;
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -27,6 +45,17 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
+
+BEGIN TRANSACTION;
+
+INSERT INTO 
+        campground (park_id, name, open_from_mm, open_to_mm, daily_fee)
+VALUES
+        ((SELECT park_id FROM park WHERE name = 'Ohiopyle State Park'), 'Youghiogheny', '01', '12', 95::money);
+        
+COMMIT;
+
+ROLLBACK;
 
 
 /*
@@ -41,6 +70,19 @@
 
 */
 
+BEGIN TRANSACTION;
+
+INSERT INTO 
+        site (campground_id, site_number)
+VALUES
+        ((SELECT campground_id FROM campground WHERE name = 'Youghiogheny'), 623),
+        ((SELECT campground_id FROM campground WHERE name = 'Youghiogheny'), 624),
+        ((SELECT campground_id FROM campground WHERE name = 'Youghiogheny'), 625);
+
+COMMIT;
+
+ROLLBACK;
+
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -52,15 +94,45 @@
 
 */
 
+BEGIN TRANSACTION;
+
+INSERT INTO 
+        reservation(site_id, name, from_date, to_date, create_date)
+VALUES
+        ((SELECT site_id FROM site WHERE site_number = 623), 'Wayne Family', '2020/10/29'::date, '2020/11/08'::date, current_date),
+        ((SELECT site_id FROM site WHERE site_number = 624), 'Parker Family', '2020/10/30'::date, '2020/11/08'::date, current_date),
+        ((SELECT site_id FROM site WHERE site_number = 625), 'Kent Family', '2020/10/31'::date, '2020/11/08'::date, current_date);
+        
+COMMIT;
+
+ROLLBACK;
+
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
 
+BEGIN TRANSACTION;
+
+UPDATE reservation SET from_date = current_date
+WHERE name = 'Wayne Family';
+
+COMMIT;
+
+ROLLBACK;
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
+
+BEGIN TRANSACTION;
+
+DELETE FROM reservation 
+WHERE name = 'Kent Family';
+
+COMMIT;
+
+ROLLBACK;
 
