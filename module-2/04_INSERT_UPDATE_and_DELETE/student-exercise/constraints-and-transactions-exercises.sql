17d5e9ec323b9d4d99d98977143f3851dfbdb9e3
Ben Squier <squier.ben@gmail.com>
2020-10-19 14:16:13 -0400

    Completed INSERT UPDATE and DELETE Exercise

--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -9,8 +9,9 @@ INSERT INTO actor (first_name, last_name) VALUES ('Hampton', 'Avenue'),('Lisa', 
 
 RETURNING *;
 
-ROLLBACK;
+COMMIT;
 
+ROLLBACK;
 
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
@@ -19,12 +20,13 @@ ROLLBACK;
 
 BEGIN TRANSACTION; 
 
-INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating) VALUES ('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', 2008, 1, 7, 4.99, 198, 29.99, 'G');
+INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating) VALUES ('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', 2008, 1, 7, 4.99, 198, 29.99, 'G')
 
 RETURNING *;
 
-ROLLBACK;
+COMMIT;
 
+ROLLBACK;
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
 
@@ -38,42 +40,76 @@ VALUES
 
 RETURNING *;
 
+COMMIT;
+
 ROLLBACK;
 
 --SELECT * from film_actor;
 
 -- 4. Add Mathmagical to the category table.
+
 BEGIN TRANSACTION; 
 
-INSERT INTO category (name) VALUES ('Mathmagical');
+INSERT INTO category (name) VALUES ('Mathmagical')
 
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
-
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
-BEGIN TRANSACTION; 
 
-INSERT INTO film_category (film_id, film_category) VALUES (film_id = SELECT film_id FROM film WHERE title = 'Euclidean PI'
+BEGIN TRANSACTION;
+
+INSERT INTO 
+        film_category(film_id,category_id)
+VALUES 
+        ((SELECT film_id FROM film WHERE  title = 'Euclidean PI'),(SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE  title = 'EGG IGBY'),(SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE  title = 'KARATE MOON'),(SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE  title = 'RANDOM GO'),(SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE  title = 'YOUNG LANGUAGE'),(SELECT category_id FROM category WHERE name = 'Mathmagical'))
 
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
-BEGIN TRANSACTION; 
 
+BEGIN TRANSACTION;
+
+UPDATE 
+        film 
+SET 
+        rating = 'G'
+WHERE 
+        film_id IN
+                (SELECT film_id FROM film_category WHERE category_id =
+                (SELECT category_id FROM category WHERE name = 'Mathmagical'))
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+
 BEGIN TRANSACTION; 
 
+INSERT INTO 
+        inventory(film_id,store_id)
+VALUES
+        ((SELECT film_id FROM film WHERE title = 'Euclidean PI'),1),
+        ((SELECT film_id FROM film WHERE title = 'Euclidean PI'),2)
+        
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
 
@@ -81,46 +117,67 @@ ROLLBACK;
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+--<No it did not work because it violates foreign key cinstraint.>
 BEGIN TRANSACTION; 
 
+DELETE FROM film WHERE title = 'Euclidean PI'
+
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
 
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
-BEGIN TRANSACTION; 
+--<No it did not work because it violates foreign key cinstraint.>
+
+BEGIN TRANSACTION;
+
+DELETE FROM category WHERE name = 'Mathmagical'
 
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
 
 -- 10. Delete all links to Mathmagical in the film_category tale.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <Yes it works because it was a primary key.>
+
 BEGIN TRANSACTION; 
 
+DELETE FROM film_category WHERE category_id = (SELECT category_id FROM category WHERE name = 'Mathmagical')
+
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
 -- (Did either deletes succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <The first delete works because its no longer being used by the foreign key in the film category table, the second delete fails for the same reason it is still being used by film_category
+>
+
 BEGIN TRANSACTION; 
 
+DELETE FROM category WHERE name = 'Mathmagical'
+
+DELETE FROM film WHERE title = 'Euclidean PI'
+
 RETURNING *;
+
+COMMIT;
 
 ROLLBACK;
 
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
-BEGIN TRANSACTION; 
-
-RETURNING *;
-
-ROLLBACK;
+/*
+ first we would have to delete the film reference from tables film_category and film_actor before deleting the film itself.  
+ This is because film_id is a primary key in these two tables.*/
+ 
