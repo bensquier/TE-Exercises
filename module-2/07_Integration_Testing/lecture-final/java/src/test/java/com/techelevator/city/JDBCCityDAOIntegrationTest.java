55e6365da32790955965a5009885ca61c69bbbc5
Ben Squier <squier.ben@gmail.com>
2020-10-24 14:15:23 -0400

    Completed Intergration Testing Lecture Notes

--- module-2/07_Integration_Testing/lecture-final/java/src/test/java/com/techelevator/city/JDBCCityDAOIntegrationTest.java
+++ module-2/07_Integration_Testing/lecture-final/java/src/test/java/com/techelevator/city/JDBCCityDAOIntegrationTest.java
@@ -50,7 +50,8 @@ public class JDBCCityDAOIntegrationTest {
 
 	@Before
 	public void setup() {
-		String sqlInsertCountry = "INSERT INTO country (code, name, continent, region, surfacearea, indepyear, population, lifeexpectancy, gnp, gnpold, localname, governmentform, headofstate, capital, code2) VALUES (?, 'Afghanistan', 'Asia', 'Southern and Central Asia', 652090, 1919, 22720000, 45.9000015, 5976.00, NULL, 'Afganistan/Afqanestan', 'Islamic Emirate', 'Mohammad Omar', 1, 'AF')";
+		String sqlInsertCountry = "INSERT INTO country (code, name, continent, region, surfacearea, indepyear, population, lifeexpectancy, gnp, gnpold, localname, governmentform, headofstate, capital, code2) " +
+				"VALUES (?, 'Afghanistan', 'Asia', 'Southern and Central Asia', 652090, 1919, 22720000, 45.9000015, 5976.00, NULL, 'Afganistan/Afqanestan', 'Islamic Emirate', 'Mohammad Omar', 1, 'AF')";
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 		jdbcTemplate.update(sqlInsertCountry, TEST_COUNTRY);
 		dao = new JDBCCityDAO(dataSource);
